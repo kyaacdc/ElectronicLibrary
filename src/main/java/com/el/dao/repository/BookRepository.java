ac53cb322d4ce91635fcd059bda6a221fbfbba27
Yuriy <kya@bk.ru>
2017-01-25 22:57:50 +0200

    Modify BookRepository.class - add methods for search book by name and ID

--- src/main/java/com/el/dao/repository/BookRepository.java
+++ src/main/java/com/el/dao/repository/BookRepository.java
@@ -2,6 +2,16 @@ package com.el.dao.repository;
 
 import com.el.dao.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
+
+    @Query(value = "SELECT b FROM Book b WHERE b.name = :name", nativeQuery = true)
+    Book findByPhoneQuery(@Param("phone") String phone);
+
+    Book findByName(String name);
+
+    Book findById(int id);
+
 }
